f76805f13250df74abdb6046cb45755e949d66cb
MartinV <marcar15@hotmail.com>
2023-06-28 21:54:08 -0300

    actualizacion de rama

--- Tecnicatura3_js/Leccion05/05-03-clases.js
+++ Tecnicatura3_js/Leccion05/05-03-clases.js
@@ -1,11 +1,25 @@
 class Persona {
     constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this._nombre = nombre;
+        this._apellido = apellido;
     }
+    get nombre(){
+        return this._nombre;
+    }
+    get apellido(){
+        return this._apellido;
+    }
+
+    get nombrecompleto(){ 
+        return this._nombre+ ' '+this._apellido;
+    };
+    
 }
 
 let persona1 = new Persona('Juan', 'Perez');
-console.log(persona1);
+console.log(persona1.nombre);
+console.log(persona1.apellido);
+console.log(persona1.nombrecompleto);
+
 let persona2 = new Persona('Luis', 'Gonzalez');
-console.log(persona2);
+console.log(persona2.nombrecompleto);
